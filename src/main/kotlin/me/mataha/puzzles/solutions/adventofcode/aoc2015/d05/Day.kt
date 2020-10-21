0e611fa8f6b591abd8a866f060ef14acc4f538f8
mataha <mataha@users.noreply.github.com>
2020-10-22 01:48:27 +0200

    Get rid of Companion in AoC2015.05

--- src/main/kotlin/me/mataha/puzzles/solutions/adventofcode/aoc2015/d05/Day.kt
+++ src/main/kotlin/me/mataha/puzzles/solutions/adventofcode/aoc2015/d05/Day.kt
@@ -13,46 +13,43 @@ import me.mataha.puzzles.util.extensions.zipWithAfterNext
 @AdventOfCode("Doesn't He Have Intern-Elves For This?", 2015, 5)
 class NoInternElvesForThis: AdventOfCodeDay<List<String>, Int>(), NoOpParser
 {
-    override fun first(input: List<String>): Int = input.count { string -> string.isNiceBefore() }
+    override fun first(input: List<String>): Int = input.count(String::isNiceBefore)
 
-    override fun second(input: List<String>): Int = input.count { string -> string.isNiceAfter() }
+    override fun second(input: List<String>): Int = input.count(String::isNiceAfter)
+}
 
-    @VisibleForTesting
-    internal companion object
-    {
-        @VisibleForTesting
-        internal fun String.isNiceBefore(): Boolean
-        {
-            val forbidden = arrayOf("ab", "cd", "pq", "xy")
-            val vowels = "aeiou"
-
-            val predicates = arrayOf<(string: String) -> Boolean>(
-                { string -> string.count { it in vowels } >= 3 },
-                { string -> string.zipWithNext().any { it.first == it.second } },
-                { string -> forbidden.none { it in string } }
-            )
-
-            return this.isNice(*predicates)
+private fun String.isNice(vararg predicates: (string: String) -> Boolean): Boolean
+        = runBlocking {
+            predicates
+                .asFlow()
+                .map { predicate -> predicate(this@isNice) }
+                .toList()
+                .none { condition -> !condition }
         }
 
-        @VisibleForTesting
-        internal fun String.isNiceAfter(): Boolean
-        {
-            val predicates = arrayOf<(string: String) -> Boolean>(
-                { string -> string.windowed(2).any { it in string.replaceFirst(it, "  ") } },
-                { string -> string.zipWithAfterNext().any { it.first == it.second } }
-            )
+private const val VOWELS = "aeiou"
 
-            return this.isNice(*predicates)
-        }
+@VisibleForTesting
+internal fun String.isNiceBefore(): Boolean
+{
+    val forbidden = arrayOf("ab", "cd", "pq", "xy")
 
-        private fun String.isNice(vararg predicates: (string: String) -> Boolean): Boolean
-                = runBlocking {
-                    predicates
-                        .asFlow()
-                        .map { predicate -> predicate(this@isNice) }
-                        .toList()
-                        .none { condition -> !condition }
-                }
-    }
+    val predicates = arrayOf<(string: String) -> Boolean>(
+        { string -> string.count { it in VOWELS } >= 3 },
+        { string -> string.zipWithNext().any { it.first == it.second } },
+        { string -> forbidden.none { it in string } }
+    )
+
+    return this.isNice(*predicates)
+}
+
+@VisibleForTesting
+internal fun String.isNiceAfter(): Boolean
+{
+    val predicates = arrayOf<(string: String) -> Boolean>(
+        { string -> string.windowed(2).any { it in string.replaceFirst(it, "  ") } },
+        { string -> string.zipWithAfterNext().any { it.first == it.second } }
+    )
+
+    return this.isNice(*predicates)
 }
