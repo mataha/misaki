eb5b5824ca6e7186cc3151825880818e8849f821
mataha <mataha@users.noreply.github.com>
2020-10-21 18:46:38 +0200

    Rewrite AoC2015.05 to be testable

--- src/main/kotlin/me/mataha/puzzles/solutions/adventofcode/aoc2015/d05/Day.kt
+++ src/main/kotlin/me/mataha/puzzles/solutions/adventofcode/aoc2015/d05/Day.kt
@@ -7,39 +7,44 @@ import kotlinx.coroutines.runBlocking
 import me.mataha.puzzles.domain.NoOpParser
 import me.mataha.puzzles.domain.adventofcode.AdventOfCode
 import me.mataha.puzzles.domain.adventofcode.AdventOfCodeDay
+import me.mataha.puzzles.util.annotations.VisibleForTesting
 import me.mataha.puzzles.util.extensions.zipWithAfterNext
 
 @AdventOfCode("Doesn't He Have Intern-Elves For This?", 2015, 5)
 class NoInternElvesForThis: AdventOfCodeDay<List<String>, Int>(), NoOpParser
 {
-    override fun first(input: List<String>): Int
+    override fun first(input: List<String>): Int = input.count { string -> string.isNiceBefore() }
+
+    override fun second(input: List<String>): Int = input.count { string -> string.isNiceAfter() }
+
+    @VisibleForTesting
+    internal companion object
     {
-        val forbidden = arrayOf("ab", "cd", "pq", "xy")
-        val vowels = "aeiou"
+        @VisibleForTesting
+        internal fun String.isNiceBefore(): Boolean
+        {
+            val forbidden = arrayOf("ab", "cd", "pq", "xy")
+            val vowels = "aeiou"
 
-        val predicates = arrayOf<(string: String) -> Boolean>(
-            { string -> string.filter { vowels.contains(it) }.count() >= 3 },
-            { string -> string.zipWithNext().any { it.first == it.second } },
-            { string -> forbidden.none { string.contains(it) } }
-        )
+            val predicates = arrayOf<(string: String) -> Boolean>(
+                { string -> string.filter { it in vowels }.count() >= 3 },
+                { string -> string.zipWithNext().any { it.first == it.second } },
+                { string -> forbidden.none { it in string } }
+            )
 
-        return input.nice(*predicates).count()
-    }
+            return this.isNice(*predicates)
+        }
 
-    override fun second(input: List<String>): Int
-    {
-        val predicates = arrayOf<(string: String) -> Boolean>(
-            { string -> string.windowed(2).any { string.replaceFirst(it, "==").contains(it) } },
-            { string -> string.zipWithAfterNext().any { it.first == it.second } }
-        )
+        @VisibleForTesting
+        internal fun String.isNiceAfter(): Boolean
+        {
+            val predicates = arrayOf<(string: String) -> Boolean>(
+                { string -> string.windowed(2).any { it in string.replaceFirst(it, "==") } },
+                { string -> string.zipWithAfterNext().any { it.first == it.second } }
+            )
 
-        return input.nice(*predicates).count()
-    }
-
-    private companion object
-    {
-        private fun List<String>.nice(vararg predicates: (string: String) -> Boolean): List<String>
-                = this.filter { string -> string.isNice(*predicates) }
+            return this.isNice(*predicates)
+        }
 
         private fun String.isNice(vararg predicates: (string: String) -> Boolean): Boolean
                 = runBlocking {
