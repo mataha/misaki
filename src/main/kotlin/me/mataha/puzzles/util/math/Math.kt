e49b8b7d19e4487cf327dec8819a6bc43671f349
mataha <mataha@users.noreply.github.com>
2020-10-22 03:08:18 +0200

    Document `min()` and `max()`

--- src/main/kotlin/me/mataha/puzzles/util/math/Math.kt
+++ src/main/kotlin/me/mataha/puzzles/util/math/Math.kt
@@ -2,8 +2,10 @@
 
 package me.mataha.puzzles.util.math
 
+/** Returns the smallest value among the supplied [Int] values. */
 fun min(number: Int, vararg numbers: Int): Int
         = kotlin.math.min(numbers.minOrNull() ?: Int.MAX_VALUE, number)
 
+/** Returns the largest value among the supplied [Int] values. */
 fun max(number: Int, vararg numbers: Int): Int
         = kotlin.math.max(numbers.maxOrNull() ?: Int.MIN_VALUE, number)
