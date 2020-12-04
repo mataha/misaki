ca22e1db239ec9321f7692fc8faff8e571cdb30f
mataha <mataha@users.noreply.github.com>
2020-12-04 16:40:52 +0100

    Make `EqualityComparator` a functional interface instead

--- app/src/main/kotlin/me/mataha/misaki/service/Comparator.kt
+++ app/src/main/kotlin/me/mataha/misaki/service/Comparator.kt
@@ -1,11 +1,5 @@
 package me.mataha.misaki.service
 
-import me.mataha.misaki.util.text.levenshteinDistance as distance
-
-internal interface EqualityComparator<in T> {
+internal fun interface EqualityComparator<in T> {
     fun compare(a: T, b: T): Boolean
-}
-
-internal class FuzzyEqualityComparator(private val threshold: Int) : EqualityComparator<String> {
-    override fun compare(a: String, b: String): Boolean = distance(a.toLowerCase(), b.toLowerCase()) <= threshold
 }
--- app/src/main/kotlin/me/mataha/misaki/service/Module.kt
+++ app/src/main/kotlin/me/mataha/misaki/service/Module.kt
@@ -1,12 +1,18 @@
 package me.mataha.misaki.service
 
+import me.mataha.misaki.util.text.levenshteinDistance
 import org.koin.dsl.module
 
-private const val THRESHOLD_KEY = "misaki.app.service.fuzzy_comparer.threshold"
+private const val THRESHOLD_KEY = "misaki.app.service.comparator.threshold"
 
 internal val module = module {
     single<EqualityComparator<String>> {
-        FuzzyEqualityComparator(getProperty(THRESHOLD_KEY).toInt())
+        EqualityComparator { a, b ->
+            levenshteinDistance(
+                a.toLowerCase(),
+                b.toLowerCase()
+            ) <= getProperty(THRESHOLD_KEY).toInt()
+        }
     }
     single<SolutionService> {
         DefaultSolutionService(get(), get())
