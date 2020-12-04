04988a40a7e5d5e2f44a4360f4a64e3cd1a35b57
mataha <mataha@users.noreply.github.com>
2020-12-04 16:58:35 +0100

    Get rid of unnecessary functions in `AdventOfCodeDay`

--- app/src/main/kotlin/me/mataha/misaki/domain/adventofcode/AdventOfCodeDay.kt
+++ app/src/main/kotlin/me/mataha/misaki/domain/adventofcode/AdventOfCodeDay.kt
@@ -6,19 +6,9 @@ abstract class AdventOfCodeDay<I : Any, out O : Any> : Solution<I, AdventOfCodeR
     override fun solve(input: I): AdventOfCodeResults<O, O> =
         AdventOfCodeResults(first(input), second(input))
 
-    override fun process(input: List<String>): AdventOfCodeResults<O, O> {
-        val parsed = parse(input)
-
-        return AdventOfCodeResults(first(parsed), second(parsed))
-    }
-
     abstract fun first(input: I): O
 
-    fun processFirst(input: List<String>): O = first(parse(input))
-
     abstract fun second(input: I): O
-
-    fun processSecond(input: List<String>): O = second(parse(input))
 }
 
 data class AdventOfCodeResults<out A, out B>(val first: A, val second: B) {
