c073e8f70351a03f0031ea405f4639e609a31243
mataha <mataha@users.noreply.github.com>
2020-12-13 03:06:41 +0100

    Create tests for AoC2015.08

---
+++ app/src/test/kotlin/me/mataha/misaki/solutions/adventofcode/aoc2015/d08/DayTests.kt
@@ -0,0 +1,40 @@
+package me.mataha.misaki.solutions.adventofcode.aoc2015.d08
+
+import io.kotest.matchers.ints.shouldBeExactly
+import me.mataha.misaki.domain.adventofcode.processFirst
+import me.mataha.misaki.domain.adventofcode.processSecond
+import me.mataha.misaki.solutions.adventofcode.AdventOfCodeTest
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvFileSource
+
+@AdventOfCodeTest
+class MatchsticksTests {
+    @Nested
+    inner class PartOne {
+        @ParameterizedTest(name = "For example, {1} - {2} given ''{0}''.")
+        @CsvFileSource(resources = ["/solutions/adventofcode/2015/08/literal-memory.csv"], numLinesToSkip = 1)
+        fun `Should return length in code minus length in memory for given string`(string: String, a: Int, b: Int) {
+            val solution = Matchsticks()
+
+            val expected = a - b
+            val actual = solution.processFirst(string)
+
+            actual shouldBeExactly expected
+        }
+    }
+
+    @Nested
+    inner class PartTwo {
+        @ParameterizedTest(name = "For example, {1} - {2} given ''{0}''.")
+        @CsvFileSource(resources = ["/solutions/adventofcode/2015/08/encoded-literal.csv"], numLinesToSkip = 1)
+        fun `Should return length encoded minus length in code for given string`(string: String, a: Int, b: Int) {
+            val solution = Matchsticks()
+
+            val expected = a - b
+            val actual = solution.processSecond(string)
+
+            actual shouldBeExactly expected
+        }
+    }
+}
