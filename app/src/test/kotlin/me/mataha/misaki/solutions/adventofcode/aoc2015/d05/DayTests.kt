329e95565a704e2e5e2d737506c446c53907f822
mataha <mataha@users.noreply.github.com>
2020-12-13 03:06:12 +0100

    Create tests for AoC2015.05

---
+++ app/src/test/kotlin/me/mataha/misaki/solutions/adventofcode/aoc2015/d05/DayTests.kt
@@ -0,0 +1,59 @@
+package me.mataha.misaki.solutions.adventofcode.aoc2015.d05
+
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import me.mataha.misaki.domain.adventofcode.processFirst
+import me.mataha.misaki.domain.adventofcode.processSecond
+import me.mataha.misaki.solutions.adventofcode.AdventOfCodeTest
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+@AdventOfCodeTest
+class NoInternElvesForThisTests {
+    @Nested
+    inner class PartOne {
+        @ParameterizedTest(name = "For example, ''{0}''.")
+        @ValueSource(strings = ["ugknbfddgicrmopn", "aaa"])
+        fun `Under old rules, given string should be nice`(string: String) {
+            val solution = NoInternElvesForThis()
+
+            val actual = solution.processFirst(string) == 1
+
+            actual.shouldBeTrue()
+        }
+
+        @ParameterizedTest(name = "For example, ''{0}''.")
+        @ValueSource(strings = ["jchzalrnumimnmhp", "haegwjzuvuyypxyu", "dvszwmarrgswjxmb"])
+        fun `Under old rules, given string should be naughty`(string: String) {
+            val solution = NoInternElvesForThis()
+
+            val actual = solution.processFirst(string) == 1
+
+            actual.shouldBeFalse()
+        }
+    }
+
+    @Nested
+    inner class PartTwo {
+        @ParameterizedTest(name = "For example, ''{0}''.")
+        @ValueSource(strings = ["qjhvhtzxzqqjkmpb", "xxyxx"])
+        fun `Under new rules, given string should be nice`(string: String) {
+            val solution = NoInternElvesForThis()
+
+            val actual = solution.processSecond(string) == 1
+
+            actual.shouldBeTrue()
+        }
+
+        @ParameterizedTest(name = "For example, ''{0}''.")
+        @ValueSource(strings = ["uurcxstgmygtbstg", "ieodomkazucvgmuy"])
+        fun `Under new rules, given string should be naughty`(string: String) {
+            val solution = NoInternElvesForThis()
+
+            val actual = solution.processSecond(string) == 1
+
+            actual.shouldBeFalse()
+        }
+    }
+}
