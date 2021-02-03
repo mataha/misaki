25c0ea7d743613f60709e1fa5555c371bf0cff61
mataha <mataha@users.noreply.github.com>
2021-02-03 20:59:40 +0100

    Upgrade Gradle Wrapper to 6.8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,12 @@
 description = "Solutions to programming koans bundled with a mini-framework"
+
+tasks.wrapper {
+    distributionType = Wrapper.DistributionType.ALL
+}
+
+allprojects {
+    repositories {
+        jcenter()
+        mavenCentral()
+    }
+}
