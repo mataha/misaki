333fbcb97c5ee537162ed20853b7c9ae303c19d8
mataha <mataha@users.noreply.github.com>
2021-02-06 03:33:55 +0100

    Use `createInstance()` instead of `instance()`

--- app/src/main/kotlin/me/mataha/misaki/domain/adventofcode/AdventOfCodeDataFactory.kt
+++ app/src/main/kotlin/me/mataha/misaki/domain/adventofcode/AdventOfCodeDataFactory.kt
@@ -4,13 +4,13 @@ import me.mataha.misaki.domain.PuzzleData
 import me.mataha.misaki.domain.PuzzleDataFactory
 import me.mataha.misaki.domain.Solution
 import me.mataha.misaki.util.reflection.findAnnotation
-import me.mataha.misaki.util.reflection.instance
 import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
 
 object AdventOfCodeDataFactory : PuzzleDataFactory {
     override fun create(solution: KClass<out Solution<*, *>>): PuzzleData<*, *> {
         val metadata = solution.findAnnotation<AdventOfCode>()
-        val instance = solution.instance()
+        val instance = solution.createInstance()
 
         return AdventOfCodeData(
             name = metadata.task,
