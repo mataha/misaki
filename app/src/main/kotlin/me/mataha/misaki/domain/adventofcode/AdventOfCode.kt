ee13780a6e42996a9a60f167770f169036aa3f02
mataha <mataha@users.noreply.github.com>
2021-02-07 05:41:44 +0100

    Add some shortnames to `AdventOfCode`

--- app/src/main/kotlin/me/mataha/misaki/domain/adventofcode/AdventOfCode.kt
+++ app/src/main/kotlin/me/mataha/misaki/domain/adventofcode/AdventOfCode.kt
@@ -4,7 +4,7 @@ import me.mataha.misaki.domain.Puzzle
 import kotlin.annotation.AnnotationTarget.CLASS
 
 @Target(CLASS)
-@Puzzle(origin = AdventOfCode.NAME, factory = AdventOfCodeDataFactory::class)
+@Puzzle(origin = AdventOfCode.NAME, handles = ["aoc", "adventofcode"], factory = AdventOfCodeDataFactory::class)
 annotation class AdventOfCode(val task: String, val year: Int, val day: Int) {
     companion object {
         const val NAME = "Advent of Code"
